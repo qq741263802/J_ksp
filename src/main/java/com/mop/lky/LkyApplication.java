5906ff2700fdd5fa539bee97832263d3e25bc09b
741263802 <741263802@qq.com>
2020-05-19 16:21:29 +0800

    初始化项目

---
+++ src/main/java/com/mop/lky/LkyApplication.java
@@ -0,0 +1,13 @@
+package com.mop.lky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LkyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LkyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mop/lky/LkyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mop.lky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LkyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
